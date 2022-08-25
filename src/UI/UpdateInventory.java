5049f966ab2a939de3dfec5770c0e5b2fc5c096a
CaptaiN785 <thakurmukeshkumar785@gmail.com>
2022-08-26 01:43:27 +0530

    added a table to updating the values of quantity of medicines.

--- src/UI/UpdateInventory.java
+++ src/UI/UpdateInventory.java
@@ -2,10 +2,17 @@ package UI;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 import Database.DatabaseOperations;
 
 import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.util.HashMap;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
 
@@ -24,9 +31,12 @@ public class UpdateInventory extends JPanel{
 	Object[][] medicineList;
 	String [] medicineNames;
 	
+	HashMap<Integer, Integer> map;
+	
 	public UpdateInventory(Frame parentFrame) {
 		this.parentFrame = parentFrame;
 		this.setLayout(new BorderLayout());
+		map = new HashMap<>();
 		
 		lbHeading = new JLabel("Update inventory");
 		lbHeading.setFont(new Font("cambria", Font.BOLD, 25));
@@ -59,5 +69,60 @@ public class UpdateInventory extends JPanel{
 		verticalLayout.add(headingPanel);
 		verticalLayout.add(searchPanel);
 		this.add(verticalLayout, BorderLayout.NORTH);
+		
+		
+		// Create a table that will allow to update the quantity of every medicine
+		// Firstly there is no data in table 
+		// Adding only column to table
+		String columns[] = {"Medicine ID", "Name", "Quantity", "Supplier ID"};
+		
+		
+		DefaultTableModel tableModel = new DefaultTableModel(columns, 0) {
+		    @Override
+		    public boolean isCellEditable(int row, int column) {
+		       //all cells false
+		       if(column == 2) return true;
+		       return false;
+		    }
+		};
+		jtTable = new JTable(tableModel);
+		jtTable.setFont(new Font("cambria", Font.PLAIN, 16));
+		jtTable.setRowHeight(18);
+		
+		// make columns not to change when it is arranged in other order
+		jtTable.getTableHeader().setReorderingAllowed(false);
+		
+		// action performed on table
+//		jtTable.setCellSelectionEnabled(true);  
+//	    ListSelectionModel select= jtTable.getSelectionModel();  
+//	    select.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);  
+//	    select.addListSelectionListener(new ListSelectionListener() {
+//	    	public void valueChanged(ListSelectionEvent e) {  
+//	            int[] row = jtTable.getSelectedRows();  
+//	            int[] column = jtTable.getSelectedColumns();  
+//	            
+//	            if(row.length == 1 && column.length == 1) {
+//	            }
+//	    	}}
+//	    );  
+
+		JScrollPane sp=new JScrollPane(jtTable);
+		this.add(sp, BorderLayout.CENTER);
+		
+		///Adding actionlistener to button to add in table
+		btnAddMedicine.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				int index = cbMedicineList.getSelectedIndex();
+				if(!map.containsKey(index)) {
+					tableModel.addRow(medicineList[index]);
+					map.put(index, 1);
+				}else {
+					JOptionPane.showMessageDialog(getRootPane(), "Medicine already added.");
+				}
+			}
+		});
+		// right click on table row to delete
+		// make quantity of each object as null
+		// 
 	}
 }
